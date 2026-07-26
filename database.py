94f3d44624168baf1d4ffa6f87f9486d2c83c555
Arav-ThewebMaker <pramilaparikh9@gmail.com>
2026-07-26 10:40:44 +0530

    Migrate backend to PostgreSQL

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
 import os
 import psycopg
+from dotenv import load_dotenv
 
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL is not set")
 
 
 def connect():
